0819a1d6ed936d21e225eebe2734b131151a6035
Alice Bevan-McGregor <alice@gothcandy.com>
2020-06-09 02:51:20 -0400

    Basic path element matching base heuristic.

--- web/security/waf.py
+++ web/security/waf.py
@@ -16,3 +16,46 @@ class WAFHeuristic:
 		lowest-overhead option to limit the impact of abuse.
 		"""
 		...
+
+
+class PathHeuristic(WAFHeuristic):
+	"""Forbid requests whose path contains a given element, or whose path matches one or more regular expressions.
+	
+	This is a _high efficiency_ heuristic, relying on Python's fast set operators where possible, while also allowing
+	regular-expression based whole-path comparisons. Simple example, prevent any request involving a path element
+	named `phpMyAdmin`:
+	
+		PathHeuristic('phpMyAdmin')
+	
+	One can also deny any request targeting a PHP script:
+	
+		PathHeuristic(re.compile(r'\.phps?($|/)'))
+	
+	It's important to note that regular expression flags (such as case insensitivity) will be ignored; the search is
+	always case sensitive.  (phpMyAdmin != phpmyadmin; these are legitimately separate resources.)
+	"""
+	
+	forbidden:Set[str]  # A set of path elements whose presence is forbidden.
+	pattern:Optional[Pattern] = None  # The compiled singular regular expression for rejection matching.
+	sensitive:bool  # Indicating if the comparison is performed case-sensitively or not.
+	
+	def __init__(self, *patterns:Iterable[Union[str,Pattern]], *, sensitive=True):
+		assert check_argument_types()
+		
+		self.forbidden = set(str(i) for i in patterns if not isinstance(i, Pattern))
+		patterns = set(patterns) - self.forbidden
+		
+		if patterns:  # For efficiency, we only want one regex. Even better is to avoid them.
+			# Construct a suitable singular regular expression from multiple expression fragments.
+			self.pattern = re(f'({")|(".join(i.pattern for i in patterns)})')
+		
+		self.sensitive = sensitive
+	
+	def __call__(self, environ:dict, uri:URI) -> None:
+		assert check_argument_types()
+		
+		if self.forbidden & set(uri.path.parts):  # This is ~a third faster than the simplest regex use.
+			raise HTTPClose()
+		
+		if self.pattern and self.pattern.search(str(uri)):  # The slower choice below a complexity threshold.
+			raise HTTPClose()
