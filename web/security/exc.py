6dd43d5eac2aa8d169c755476552634abd8cf74e
Alice Bevan-McGregor <alice@gothcandy.com>
2020-12-20 09:45:13 -0500

    Use correct code, we are not a client ourselves.

--- web/security/exc.py
+++ web/security/exc.py
@@ -4,6 +4,6 @@ from webob.exc import HTTPClientError
 class HTTPClose(HTTPClientError):
 	"""Indicate to the front-end load balancer (FELB) that it should hang up on the client."""
 	
-	code = 499
-	title = "Client Closed Request"
+	code = 444
+	title = "Connection Closed Without Response"
 	explanation = "The server did not accept your request."
