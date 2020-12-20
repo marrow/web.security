5a8ba9733d81e168d5f08ad0a691f1068b5c8242
Alice Bevan-McGregor <alice@gothcandy.com>
2020-12-20 11:52:36 -0500

    Who needs a temporary variable?

--- web/security/waf.py
+++ web/security/waf.py
@@ -281,7 +281,5 @@ class GeoCountryHeuristic(WAFHeuristic):
 	def __call__(self, environ:dict, uri:URI, client:str) -> None:
 		assert check_argument_types()
 		
-		rec = self.resolver.get_country_short(client)
-		
-		if rec in self.countries:
+		if self.resolver.get_country_short(client) in self.countries:
 			raise HTTPClose(f"Access from {self.resolver.get_country_long(client)} forbidden.")
